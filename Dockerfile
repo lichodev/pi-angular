5f50156b5f65719ed65c1e292b04f774d948265f
Lisandro Marchionni <marchionni.lisandro@gmail.com>
2023-09-11 19:32:31 -0300

    Now BASE_URL can be changed from env variables

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
 #syntax=docker/dockerfile:1
 FROM node:12-alpine
 WORKDIR /app
-COPY . .
+
+COPY package*.json .
 RUN npm install
+
+COPY . .
 RUN npm run build
 
 # stage 2
@@ -10,5 +13,7 @@ FROM nginx:alpine
 COPY --from=0 /app/dist/primeraInfancia /usr/share/nginx/html
 COPY --from=0 /app/nginx.conf /etc/nginx/conf.d/default.conf
 
+ENV BASE_URL=http://localhost:18080
 EXPOSE 80
 
+CMD ["/bin/sh",  "-c",  "envsubst < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js && exec nginx -g 'daemon off;'"]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,27 +1,33 @@
 export const environment = {
-  production: true
+  production: true,
 };
 
-export const BASE_URL = "http://localhost:18080";
+// @ts-ignore
+export const BASE_URL = window['env']['apiUrl'];
 
 /* -- ERRORS -- */
-export const SAVE_ERROR: String = "No se pudo guardar. Verificá que todos los campos estén completos.";
-export const LOGIN_ERROR: String = "Usuario y/o contraseña incorrecto(s)";
+export const SAVE_ERROR: String =
+  'No se pudo guardar. Verificá que todos los campos estén completos.';
+export const LOGIN_ERROR: String = 'Usuario y/o contraseña incorrecto(s)';
 
 /* -- SUCCESS -- */
-export const SAVED_TIP: String = "Tip guardado";
-export const SAVED_IMAGE: String = "Imagen guardada";
-export const SAVED_PODCAST: String = "Podcast guardado";
-export const SAVED_MSG: String = "Gracias por comunicarte. Pronto nos ponemos en contacto.";
-export const SAVED_EXPERIENCE: String = "Gracias por contarnos tu experiencia, cuando sea aprobada aparecerá en el listado.";
-export const SAVED_CHANGES: String = "Cambios guardados";
-export const SAVED_RESPONSE: String = "Respuesta guardada";
+export const SAVED_TIP: String = 'Tip guardado';
+export const SAVED_IMAGE: String = 'Imagen guardada';
+export const SAVED_PODCAST: String = 'Podcast guardado';
+export const SAVED_MSG: String =
+  'Gracias por comunicarte. Pronto nos ponemos en contacto.';
+export const SAVED_EXPERIENCE: String =
+  'Gracias por contarnos tu experiencia, cuando sea aprobada aparecerá en el listado.';
+export const SAVED_CHANGES: String = 'Cambios guardados';
+export const SAVED_RESPONSE: String = 'Respuesta guardada';
 
 /* -- CLASS -- */
-export const SUCCESS_CLASS: String = "success";
-export const ERROR_CLASS: String = "error";
+export const SUCCESS_CLASS: String = 'success';
+export const ERROR_CLASS: String = 'error';
 
 /* -- WARNINGS -- */
 
-export const PRIVATE_DATA: String = "Tus datos están a salvo, sólo serán visibles para los administradores.";
-export const PENDING_DATA: String = "Tu mensaje se hará visible cuando sea aprobado por un/a administrador/a";
+export const PRIVATE_DATA: String =
+  'Tus datos están a salvo, sólo serán visibles para los administradores.';
+export const PENDING_DATA: String =
+  'Tu mensaje se hará visible cuando sea aprobado por un/a administrador/a';
